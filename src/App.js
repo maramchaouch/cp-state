9c7850479d2aa97798bca046491d53a7a8ff6a91
maramchaouch <maramchaouech15@gmail.com>
2024-02-29 10:59:22 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import ts from './Taylor Swift.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+    constructor (){
+      super()
+      this.state = {
+      
+          fullName:'name',
+          Bio:'bio',
+          imgsrc:'img',
+          profession:'profession',
+          test:false
+      
+      
+      }
+    }
+    clic=()=>{
+      this.setState({test:(!this.state.test)});
+      this.setState({fullName:'maram chaouch',bio:'she/her',imgsrc:ts,profession:'student'})
+    }
+    render(){
 
-export default App;
+      return (
+        <>
+        <div className='page'>
+          
+        <div className='profil'  style={{display: this.state.test ? 'none'  : 'block'}}>
+            <p>Photo</p>
+                <p >Name</p>
+                <p>Bio</p>
+                <p>Profession</p>
+        </div>
+        <div className="profil"  style={{display: this.state.test ? 'block'  : 'none'}}>
+        <img src={this.state.imgsrc} alt='' style={{width:'150px',height:'150px'}}></img>
+          <h1>{this.state.fullName}</h1>
+          <h1>{this.state.bio}</h1>
+          <h1>{this.state.profession}</h1>
+          <h1>{this.state.test}</h1>
+          </div>
+          <div  >
+            <button className='btn' onClick={()=>this.clic()}>togggle profile</button>
+        </div>
+          </div>
+          </>
+          
+          
+          
+          
+          )
+}}
+export default App
+
